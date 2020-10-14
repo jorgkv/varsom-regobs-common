e940227d9d04cd4708eff5d82c871069d416ec85
Jørgen Kvalberg <jolokv@nve.no>
2020-10-14 20:28:59 +0200

    Test

--- packages/core/src/lib/helpers.ts
+++ packages/core/src/lib/helpers.ts
@@ -1,8 +1,8 @@
 export { toPromiseWithCancel } from './helpers/toPromiseWithCancel';
 export { getLangKeyString } from './models/lang-key.enum';
+export { enterZone } from './helpers/enter-zone';
 export { isEmpty } from './helpers/isEmpty';
 export { isNumeric, setDecimalPlaces } from './helpers/number.extensions';
 export { enterZoneAndTickApplicationRef } from './helpers/enter-zone-and-tick-application-ref';
-export { enterZone } from './helpers/enter-zone';
-export { uuidv4 } from './helpers/Uuid';
+export { uuidv4 }  from './helpers/Uuid';
 export { tickApplicationRef } from './helpers/tick-application-ref';
